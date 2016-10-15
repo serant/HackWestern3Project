9afa33b952104fe492d9a4a0776e1ff488a1dcbe
serant <seran.thirugnanam@gmail.com>
2016-10-15 02:09:34 -0400

    Fixed compilation issues and removed menu activity

--- app/src/main/java/com/example/patrick/hackwestern3project/Shopping.java
+++ app/src/main/java/com/example/patrick/hackwestern3project/Shopping.java
@@ -3,15 +3,11 @@ package com.example.patrick.hackwestern3project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Fridge extends AppCompatActivity {
+public class Shopping extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fridge);
-    }
-
-    public void goToFridge( ){
-
+        setContentView(R.layout.activity_shopping);
     }
 }
